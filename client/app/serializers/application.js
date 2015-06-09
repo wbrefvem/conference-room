608fa09a55b2e180db784d2ba4b758cd02d808ff
Will Refvem <will.refvem@raleighnc.gov>
2015-06-09 15:39:01 -0400

    Minor tweaks to accomodate new Django API server

--- client/app/serializers/application.js
+++ client/app/serializers/application.js
@@ -1,3 +1,8 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
-export default DS.RESTSerializer.extend();
+export default DS.RESTSerializer.extend({
+  keyForAttribute: function(attr) {
+    return Ember.String.underscore(attr);
+  }
+});
