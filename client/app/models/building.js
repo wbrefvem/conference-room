a9104475a97b3b7a56b4ed9931a9d59880af7751
Will Refvem <wbrefvem@gmail.com>
2015-06-23 12:53:14 -0400

    Deleted legacy files

--- client/app/models/building.js
+++
@@ -1,9 +0,0 @@
-import DS from "ember-data";
-
-var Building = DS.Model.extend({
-    name: DS.attr('string'),
-    address: DS.attr('string'),
-    rooms: DS.hasMany('room')
-});
-
-export default Building;
--- client/app/serializers/application.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-import Ember from 'ember';
-
-export default DS.RESTSerializer.extend({
-  keyForAttribute: function(attr) {
-    return Ember.String.underscore(attr);
-  }
-});
