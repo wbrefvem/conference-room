07b04e74509a24eeb570ef286c34e3d603cf45ba
Will Refvem <wbrefvem@gmail.com>
2015-06-29 15:21:08 -0400

    Heroku config stuff

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 export default DS.RESTAdapter.extend({
   coalesceFindRequests: true,
   namespace: 'api/v1',
-  host: 'http://mapststarcsrv3:8008',
+  host: 'https://conference-room-api.herokuapp.com',
 
   pathForType: function(type) {
     var dasherized = Ember.String.dasherize(type);
