313fe72dbaf6c2b0f6083fdfce2c6b8a82d711fc
Will Refvem <wbrefvem@gmail.com>
2015-06-29 15:53:02 -0400

    Fixing broken input labelling

---
+++ app/components/phone-input.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  inputElementId: null,
+  
+  init: function() {
+    this._super.apply(this, arguments);
+    
+    var elementId = this.elementId;
+    this.set('inputElementId', elementId + '-input');
+  }  
+});
