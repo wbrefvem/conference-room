30ab7e26c6bf523b701c60aa27d681cb5ef16eda
Will Refvem <wbrefvem@gmail.com>
2015-07-07 16:04:23 -0400

    Removed async: true

--- app/models/room.js
+++ app/models/room.js
@@ -1,7 +1,7 @@
 import DS from "ember-data";
 
 var Room = DS.Model.extend({
-    building: DS.belongsTo('building', { async: true }),
+    building: DS.belongsTo('building'),
     roomNumber: DS.attr('string'),
     roomType: DS.attr('string'),
     manager: DS.attr('string'),
