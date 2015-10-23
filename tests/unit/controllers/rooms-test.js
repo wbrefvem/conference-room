eb809396faf2e91486d5b8e40ed3f15fdd2db408
Will Refvem <wbrefvem@gmail.com>
2015-10-23 15:22:24 -0400

    Added new tests and exposed PhantomJS debug port

---
+++ tests/unit/controllers/rooms-test.js
@@ -0,0 +1,27 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+import DS from 'ember-data';
+
+moduleFor('controller:rooms');
+
+test('it exists', function(assert) {
+  var controller = this.subject(DS.Store.create({}));
+  assert.ok(controller);
+});
+
+/*test('it is actually a controller', function(assert) {
+});*/
+
+test('it should have a model bound to it', function(assert) {
+  var controller = this.subject();
+  assert.ok(controller.get('model'));
+});
+
+test('it should filter model based on params', function(assert) {
+  var controller = this.subject();
+  controller.set('selectedBuilding', 12);
+  assert.equal(controller.get('filteredRooms').length, 1);
+});
